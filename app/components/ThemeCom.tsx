8963bff47c4dd9ad0a6eb91da2221176698d5dfd
Rafly Ardiansyah <rafly21ardiansyah@gmail.com>
2025-03-06 15:28:27 +0700

    add next themes to apply dark mode

---
+++ app/components/ThemeCom.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import React, { type ReactNode } from "react";
+interface ThemeComProps {
+  children: ReactNode;
+}
+
+const ThemeCom = ({ children }: ThemeComProps) => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  return (
+    <div className={theme}>
+      <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16, 23, 42)] min-h-screen">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ThemeCom;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { ThemeProvider } from "next-themes";
+import ThemeCom from "./components/ThemeCom";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
+        <ThemeProvider>
+          <ThemeCom>
+            <Header />
+            {children}
+          </ThemeCom>
+        </ThemeProvider>
       </body>
     </html>
   );
